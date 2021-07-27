e3ee4201071b0aa8c447a4770a221c1cd29f5591
antares0982@gmail.com <Antares0982@gmail.com>
2021-07-27 17:55:45 +0800

    add location to data

--- checkin.py
+++ checkin.py
@@ -11,6 +11,16 @@ from Crypto.Util import Padding
 from bs4 import BeautifulSoup
 
 
+class JsonDict(dict):
+    """general json object that allows attributes to be bound to and also behaves like a dict"""
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, attr, value):
+        self[attr] = value
+
+
 def main():
     def encryptAES(_p0: str, _p1: str) -> str:
         _chars = list('ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678')
@@ -19,7 +29,8 @@ def main():
             return ''.join(random.choices(_chars, k=len))
 
         def _gas(data: str, key0: str, iv0: str) -> bytes:
-            encrypt = AES.new(key0.strip().encode('utf-8'), AES.MODE_CBC, iv0.encode('utf-8'))
+            encrypt = AES.new(key0.strip().encode('utf-8'),
+                              AES.MODE_CBC, iv0.encode('utf-8'))
             return base64.b64encode(encrypt.encrypt(Padding.pad(data.encode('utf-8'), 16)))
         return _gas(_rds(64) + _p0, _p1, _rds(16)).decode('utf-8')
 
@@ -62,7 +73,10 @@ def main():
     result = session.get('http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/saveApplyInfos.do?' +
                          '&'.join([key + '=' + data[key] for key in fields]))
 
-    print(result.text)
+    answer = json.loads(result.text, object_hook=JsonDict)
+    answer.data.location = data['CURR_LOCATION']
+    print(answer)
+
     if result.status_code != 200:
         exit(1)
 
