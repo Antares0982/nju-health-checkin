6cb4674bb7e80a9a47c51bc4c82ce667fc2e25c6
antares0982@gmail.com <Antares0982@gmail.com>
2022-04-18 17:36:31 +0800

    :new: github action manually

--- githubaction.sh
+++ githubaction.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-RESULT=$(python checkin.py)
+RESULT=$(TO_HTML=1 python checkin.py)
 
 echo "$RESULT"
 
 towho=$TELEGRAM_TO
 bottoken=$TELEGRAM_TOKEN
+
+if [[ -z $bottoken || -z $towho ]]; then
+    echo "no tgid or bot token"
+    exit
+fi
 
 # for telegram message
 curl "https://api.telegram.org/bot${bottoken}/sendMessage?chat_id=${towho}&text=${RESULT}"
